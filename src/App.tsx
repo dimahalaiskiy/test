219135018b24eb893b1cdabf059c8f1d6d73e498
Dima Halaiskiy <dgalaiskiy@icloud.com>
2022-04-29 16:34:53 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,123 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { Box, Modal, Button } from '@mui/material';
+import ListCases from './Components/ListCases';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [covidCases, setCovidCases] = useState([]);
+	const [from, setFrom] = useState(new Date());
+	const [to, setTo] = useState(new Date());
+
+	const [openFrom, setOpenFrom] = useState(false);
+	const handleOpenFrom = () => setOpenFrom(true);
+	const handleCloseFrom = () => setOpenFrom(false);
+
+	const [openTo, setOpenTo] = useState(false);
+	const handleOpenTo = () => setOpenTo(true);
+	const handleCloseTo = () => setOpenTo(false);
+
+	const style = {
+		position: 'absolute',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		width: 300,
+		bgcolor: 'background.paper',
+		border: '2px solid #000',
+		boxShadow: 24,
+		p: 4,
+	};
+
+	const getCasesFromTo: any = async (from: any, to: any) => {
+		const { data } = await axios('https://api.covid19api.com/country/belgium/status/confirmed', {
+			params: {
+				from: from.toISOString(),
+				to: to.toISOString(),
+			},
+		});
+
+		setCovidCases(data);
+	};
+
+	useEffect(() => {
+		getCasesFromTo(from, to);
+	}, []);
+
+	return (
+		<div>
+			<h2>
+				Select the date <span style={{ color: 'violet' }}>FROM</span> and
+				<span style={{ color: 'violet', marginBottom: '50px' }}> TO</span> to see the number of
+				diseases on Covid
+			</h2>
+			<Box sx={{ display: 'flex', justifyContent: 'center' }}>
+				<Box
+					sx={{
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						justifyContent: 'center',
+						marginRight: '30px',
+					}}>
+					<Button variant='contained' onClick={handleOpenFrom} sx={{ width: '100px' }}>
+						from
+					</Button>
+					<p>{from.toLocaleDateString()}</p>
+				</Box>
+				<Box
+					sx={{
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						justifyContent: 'center',
+						marginRight: '30px',
+					}}>
+					<Button variant='contained' onClick={handleOpenTo} sx={{ width: '100px' }}>
+						to
+					</Button>
+					<p>{to.toLocaleDateString()}</p>
+				</Box>
+				<Button
+					onClick={() => getCasesFromTo(from, to)}
+					variant='contained'
+					color='success'
+					sx={{ width: '100px', height: '36.7px' }}>
+					Search
+				</Button>
+			</Box>
+			<Modal
+				open={openFrom}
+				onClose={handleCloseFrom}
+				aria-labelledby='modal-modal-title'
+				aria-describedby='modal-modal-description'>
+				<Box sx={style}>
+					<Calendar onChange={setFrom} value={from} />
+					<Button sx={{ marginTop: '20px' }} onClick={handleCloseFrom}>
+						Close
+					</Button>
+				</Box>
+			</Modal>
+
+			<Modal
+				open={openTo}
+				onClose={handleCloseTo}
+				aria-labelledby='modal-modal-title'
+				aria-describedby='modal-modal-description'>
+				<Box sx={style}>
+					<Calendar onChange={setTo} value={to} />
+					<Button sx={{ marginTop: '20px' }} onClick={handleCloseTo}>
+						Close
+					</Button>
+				</Box>
+			</Modal>
+
+			<ListCases cases={covidCases} />
+		</div>
+	);
+};
 
 export default App;
---
+++ src/Components/ListCases.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ListCases = (props: any) => {
+	const { cases } = props;
+
+	const from = cases[0]?.Cases;
+	const to = cases[cases.length - 1]?.Cases;
+
+	return (
+		<div>
+			<p> {from && to && to - from} number of infected </p>
+		</div>
+	);
+};
+
+export default ListCases;
